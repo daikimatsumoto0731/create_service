736accd9f8227e8d0c86de4fcbb4cd686be53b13
Matsumoto Daiki <dai.dai.2002@icloud.com>
2023-12-29 20:52:11 +0900

    ログインで必要なファイルとi18nの導入

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module VegetableService
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.i18n.default_locale = :ja
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
