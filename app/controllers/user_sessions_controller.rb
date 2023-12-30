50763b3455fb6b637f985273d6e08fbb4f013045
Matsumoto Daiki <dai.dai.2002@icloud.com>
2023-12-30 22:36:09 +0900

    トップページファイルとルーティングの設定

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,4 +1,23 @@
-class UserSessionsController < ApplicationController
-  def index
+class UserSessionsController < Devise::SessionsController
+  def new
+    super
+  end
+    
+    # ユーザーのログイン処理
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    if resource
+      set_flash_message!(:notice, :signed_in)
+      sign_in(resource_name, resource)
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      set_flash_message!(:alert, :invalid)
+      render :new
+    end
+  end
+    
+  # ユーザーのログアウト処理
+  def destroy
+    super
   end
 end
