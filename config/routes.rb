2eed25c538c567d0e8c1a71423b1f0e673d1fc94
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-01-07 23:42:24 +0900

    ログイン画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
 
   root 'static_pages#top'
+  
+  get 'terms', to: 'static_pages#terms', as: :terms
+  get 'privacy_policy', to: 'static_pages#privacy_policy', as: :privacy_policy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
