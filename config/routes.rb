d3d34535cccd94d3d6fad83a0db62f56cf1335b6
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-01-10 22:51:04 +0900

    マイページ画面作成途中

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
     sessions: 'user_sessions',
   }
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:show]
 
   root 'static_pages#top'
   
   get 'terms', to: 'static_pages#terms', as: :terms
   get 'privacy_policy', to: 'static_pages#privacy_policy', as: :privacy_policy
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
