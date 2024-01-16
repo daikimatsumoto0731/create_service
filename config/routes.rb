95eee14e46585c4c98c05cd6f12d088bb6da2c2d
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-01-17 00:59:05 +0900

    野菜選択画面での仕様の調整

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,12 @@ Rails.application.routes.draw do
   get 'terms', to: 'static_pages#terms', as: :terms
   get 'privacy_policy', to: 'static_pages#privacy_policy', as: :privacy_policy
 
-  # 野菜選択画面へのルーティング
-  get 'vegetables', to: 'vegetables#index', as: :vegetables
+  # スケジュール画面へのルーティング
+  get 'custom_schedule', to: 'vegetables#custom_schedule', as: 'custom_schedule'
 
   # 選択ボタンを押した先のルーティング
-  post 'vegetables', to: 'vegetables#schedule', as: :schedule
+  post 'schedule', to: 'vegetables#schedule'
 
-  # スケジュール画面のルーティング
-  get 'custom_schedule', to: 'vegetables#custom_schedule', as: 'custom_schedule'
+  # 野菜選択画面へのルーティング
+  get 'vegetables', to: 'vegetables#index', as: :vegetables
 end
