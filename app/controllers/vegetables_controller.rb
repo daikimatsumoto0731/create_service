20be3bf3228863d491319cbed06319457d0b897d
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-01-17 00:24:58 +0900

    野菜選択画面の調整

--- app/controllers/vegetables_controller.rb
+++ app/controllers/vegetables_controller.rb
@@ -3,18 +3,20 @@ class VegetablesController < ApplicationController
   end
 
   def schedule
-    if params[:'selected-vegetable'].blank?
+    # 選択された野菜を取得
+    selected_vegetable = params[:selected_vegetable]
+
+    if selected_vegetable.blank?
       flash[:alert] = '野菜を選択してください'
-      render :index
+      redirect_to vegetables_path  # 選択されていない場合は野菜選択画面にリダイレクト
     else
-      # 選択された野菜を取得
-      selected_vegetable = params[:'selected-vegetable']
-      
-      # スケジュール画面にリダイレクトし、選択した野菜をパラメータとして渡す
-      redirect_to schedule_path(selected_vegetable: selected_vegetable)
+      # 選択された野菜をスケジュール画面に渡す
+      redirect_to custom_schedule_path(selected_vegetable: selected_vegetable)
     end
   end
 
   def custom_schedule
+    # スケジュール画面で選択した野菜を取得
+    @selected_vegetable = params[:selected_vegetable]
   end
 end
