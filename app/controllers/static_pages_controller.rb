a31f5f0135b1f6d9e3c56c04b16ed7ecc89cc9ca
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-01-18 23:31:47 +0900

    LINE通知画面からの画面遷移とデザイン作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
+  before_action :authenticate_user!, only: [:line_notification_settings]
+  before_action :set_user, only: [:line_notification_settings]
+
   def top; end
 
   def terms; end
@@ -6,4 +9,10 @@ class StaticPagesController < ApplicationController
   def privacy_policy; end
 
   def line_notification_settings; end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
 end
