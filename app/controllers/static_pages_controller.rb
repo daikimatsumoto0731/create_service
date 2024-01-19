53f32feead2f1fa94aceb04938a925b58bade0d2
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-01-19 19:27:04 +0900

    LINE通知画面のデザインと機能の追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
-  before_action :authenticate_user!, only: [:line_notification_settings]
-  before_action :set_user, only: [:line_notification_settings]
+  before_action :authenticate_user!
+  before_action :set_user
+  before_action :set_line_notification_setting, only: [:line_notification_settings]
 
   def top; end
 
@@ -8,11 +9,28 @@ class StaticPagesController < ApplicationController
   
   def privacy_policy; end
 
-  def line_notification_settings; end
+  def line_notification_settings
+    if request.patch?
+      if @line_notification_setting.update(line_notification_setting_params)
+        redirect_to line_notification_settings_path, notice: '設定ができました'
+      else
+        flash.now[:alert] = '設定ができませんでした'
+        render :line_notification_settings
+      end
+    end
+  end
 
   private
 
   def set_user
     @user = current_user
+  end
+
+  def set_line_notification_setting
+    @line_notification_setting = @user.line_notification_setting || @user.build_line_notification_setting
+  end
+
+  def line_notification_setting_params
+    params.require(:line_notification_setting).permit(:receive_notifications, :frequency)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   # バリデーションの追加
   validates :username, presence: true
   validates :prefecture, presence: true
+
+  # LINE通知設定モデルへの関連付け
+  has_one :line_notification_setting, dependent: :destroy
 end
