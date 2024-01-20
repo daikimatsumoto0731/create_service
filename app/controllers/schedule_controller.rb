e936509515e06195f65a0ae0363dd77ab62c5da8
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-01-20 17:46:28 +0900

    スケジュール機能作成途中

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,4 +1,16 @@
 class ScheduleController < ApplicationController
-  def index
+  def custom_schedule
+    @selected_vegetable = params[:selected_vegetable]
+    
+    # Railsのログにパラメータとテンプレートの存在を出力
+    Rails.logger.debug "Selected vegetable: #{@selected_vegetable}"
+    template_exists = lookup_context.template_exists?(@selected_vegetable, "schedule", false)
+    Rails.logger.debug "Template exists for #{@selected_vegetable}: #{template_exists}"
+
+    if template_exists
+      render template: "schedule/#{@selected_vegetable}"
+    else
+      render 'schedule/default_schedule'
+    end
   end
 end
