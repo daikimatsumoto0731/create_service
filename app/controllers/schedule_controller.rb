86bbeef4b1bd784e0a3ba6eedb2b9431fd88fcd0
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-01-22 22:52:40 +0900

    トマト、にんじん、バジルの画面遷移とトマトのスケジュール作成

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,16 +1,18 @@
 class ScheduleController < ApplicationController
   def custom_schedule
-    @selected_vegetable = params[:selected_vegetable]
-    
-    # Railsのログにパラメータとテンプレートの存在を出力
-    Rails.logger.debug "Selected vegetable: #{@selected_vegetable}"
-    template_exists = lookup_context.template_exists?(@selected_vegetable, "schedule", false)
-    Rails.logger.debug "Template exists for #{@selected_vegetable}: #{template_exists}"
+    @selected_vegetable = params[:selected_vegetable].downcase
 
-    if template_exists
+    vegetable = Vegetable.find_by(name: @selected_vegetable)
+    @schedules = vegetable ? vegetable.schedules : Schedule.none
+
+    @selected_vegetable_name = case @selected_vegetable
+                                when "tomato" then "トマト"
+                                when "carrot" then "ニンジン"
+                                when "basil" then "バジル"
+                                end
+
+    if vegetable.present?  # 野菜が存在する場合のみ該当のビューをレンダリング
       render template: "schedule/#{@selected_vegetable}"
-    else
-      render 'schedule/default_schedule'
     end
   end
 end
