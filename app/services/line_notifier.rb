970fe5bf53f6868453dff92fd134ec74b8805b8e
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-01-25 22:31:37 +0900

    LINE通知機能途中

--- app/services/line_notifier.rb
+++ app/services/line_notifier.rb
@@ -11,27 +11,31 @@ class LineNotifier
   
     # アクセストークンを環境変数から取得
     api_key = ENV['LINE_NOTIFY_API_KEY']
+    unless api_key
+      Rails.logger.error("LINE_NOTIFY_API_KEY is not set.")
+      return
+    end
   
     response = self.class.post('/api/notify',
       body: {
-       message: message
+        message: message
       }.to_json,
       headers: {
         'Content-Type' => 'application/json',
-          'Authorization' => "Bearer #{api_key}"
+        'Authorization' => "Bearer #{api_key}"
       }
     )
   
-      # レスポンスの確認とエラーハンドリング
     handle_response(response)
   end
   
   private
   
   def handle_response(response)
-    if response.code != 200
-      Rails.logger.error("LINE API Error: #{response.body}")
+    if response.code == 200
+      Rails.logger.info("LINE notification sent successfully.")
+    else
+      Rails.logger.error("LINE API Error: Status: #{response.code}, Response: #{response.body}")
     end
   end
 end
-  
