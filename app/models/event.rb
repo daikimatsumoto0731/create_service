c7014d32f4a94a84a84a658ab2fcd6735c0fe77c
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-02-19 22:53:59 +0900

    スケジュール管理機能作成中

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,14 @@ class Event < ApplicationRecord
   # バリデーション
   validates :name, presence: true
   validate :end_date_after_start_date, if: -> { start_date.present? && end_date.present? }
+
+  # イベントの期間（日数）を計算するメソッド
+  def duration_in_days
+    # start_date と end_date の両方が存在する場合のみ計算を行う
+    if start_date && end_date
+      (end_date - start_date).to_i
+    end
+  end
   
   private
   
