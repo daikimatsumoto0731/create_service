92593064c8a33515e7c114ed70039b2bac0f8b22
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-02-19 22:53:59 +0900

    スケジュール管理機能作成中

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-vegetable = Vegetable.find_or_create_by(name: 'バジル')
-Event.create([
-  { vegetable: vegetable, name: '種まき', start_date: Date.today, end_date: Date.today + 7, description: '種まきに関するアドバイス' },
-  { vegetable: vegetable, name: '発芽期間', start_date: Date.today + 8, end_date: Date.today + 14, description: '発芽期間に関するアドバイス' },
-  { vegetable: vegetable, name: '間引き・雑草抜き・害虫駆除', start_date: Date.today + 15, end_date: Date.today + 21, description: '間引き・雑草抜き・害虫駆除に関するアドバイス' },
-  { vegetable: vegetable, name: '収穫期間', start_date: Date.today + 22, end_date: Date.today + 28, description: '収穫期間に関するアドバイス' },
-])
+# 野菜の作成または取得
+basil = Vegetable.find_or_create_by(name: 'バジル')
+carrot = Vegetable.find_or_create_by(name: 'にんじん')
+tomato = Vegetable.find_or_create_by(name: 'トマト')
+
+# バジルのイベント
+Event.create(vegetable: basil, name: "種まき", start_date: Date.today, end_date: Date.today + 5.days)
+Event.create(vegetable: basil, name: "発芽期間", start_date: Date.today + 5.days, end_date: Date.today + 25.days)
+Event.create(vegetable: basil, name: "間引き・雑草抜き・害虫駆除", start_date: Date.today + 25.days, end_date: Date.today + 65.days)
+Event.create(vegetable: basil, name: "収穫期間", start_date: Date.today + 65.days, end_date: Date.today + 105.days)
+
+# にんじんのイベント
+Event.create(vegetable: carrot, name: "種まき", start_date: Date.today, end_date: Date.today + 10.days)
+Event.create(vegetable: carrot, name: "発芽期間", start_date: Date.today + 10.days, end_date: Date.today + 40.days)
+Event.create(vegetable: carrot, name: "間引き・雑草抜き・害虫駆除", start_date: Date.today + 40.days, end_date: Date.today + 90.days)
+Event.create(vegetable: carrot, name: "収穫期間", start_date: Date.today + 90.days, end_date: Date.today + 170.days)
+
+# トマトのイベント
+Event.create(vegetable: tomato, name: "種まき", start_date: Date.today, end_date: Date.today + 5.days)
+Event.create(vegetable: tomato, name: "発芽期間", start_date: Date.today + 5.days, end_date: Date.today + 35.days)
+Event.create(vegetable: tomato, name: "間引き・雑草抜き・害虫駆除", start_date: Date.today + 35.days, end_date: Date.today + 95.days)
+Event.create(vegetable: tomato, name: "収穫期間", start_date: Date.today + 95.days, end_date: Date.today + 175.days)
