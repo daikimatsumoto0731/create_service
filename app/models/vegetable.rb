2106619fda117ee087402b9b26f95474e0ed0f4f
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-02-21 23:45:50 +0900

    種まき日を更新すると更新されるようにする機能作成

--- app/models/vegetable.rb
+++ app/models/vegetable.rb
@@ -3,17 +3,16 @@ class Vegetable < ApplicationRecord
 
   # 種まき日を基にして関連するイベントの日付を更新するメソッド
   def update_related_event_dates
-    # 各イベント名に対応する日付の更新
     events.each do |event|
       case event.name
       when "種まき"
-        # 種まきイベントは既に設定されているので、ここでは何もしません
+        event.update!(start_date: sowing_date, end_date: sowing_date + 5.days)
       when "発芽期間"
-        event.update(start_date: sowing_date + 5.days, end_date: sowing_date + 25.days)
+        event.update!(start_date: sowing_date + 5.days, end_date: sowing_date + 25.days)
       when "間引き・雑草抜き・害虫駆除"
-        event.update(start_date: sowing_date + 25.days, end_date: sowing_date + 65.days)
+        event.update!(start_date: sowing_date + 25.days, end_date: sowing_date + 65.days)
       when "収穫期間"
-        event.update(start_date: sowing_date + 65.days, end_date: sowing_date + 105.days)
+        event.update!(start_date: sowing_date + 65.days, end_date: sowing_date + 105.days)
       end
     end
   end
