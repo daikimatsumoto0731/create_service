526d4b1fa361c845e601df7c5a92fa8a55c030e9
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-02-27 23:56:26 +0900

    ビューのデザインとマイページに収穫したデータを表示

--- app/controllers/harvests_controller.rb
+++ app/controllers/harvests_controller.rb
@@ -1,39 +1,59 @@
 class HarvestsController < ApplicationController
   before_action :authenticate_user!
-
+  
   def new
-    @harvest = current_user.harvests.build
-    set_vegetable_info
-  end
+    @harvest = Harvest.new
+    @vegetable_type = params[:vegetable_type] || "バジル"
+    set_price_per_kg(@vegetable_type)
+    render_template(@vegetable_type)
+  end  
 
   def create
-    @harvest = current_user.harvests.build(harvest_params)
+    @harvest = current_user.harvests.new(harvest_params) # 現在のユーザーに関連付け
     if @harvest.save
-      redirect_to user_path(current_user), notice: '収穫情報を保存しました。'
+      redirect_to harvest_path(@harvest) # 単数形に修正
     else
-      set_vegetable_info
-      render :new
+      @vegetable_type = @harvest.vegetable_type
+      set_price_per_kg(@vegetable_type)
+      flash.now[:alert] = "入力に誤りがあります。"
+      render_template(@vegetable_type)
     end
+  end  
+
+  def show
+    @harvest = Harvest.find(params[:id])
+    # 節約額の計算
+    @savings = @harvest.amount * @harvest.price_per_kg
+  end
+
+  def destroy_by_vegetable_type
+    @user = current_user
+    @user.harvests.where(vegetable_type: params[:vegetable_type]).destroy_all
+    redirect_to user_path(@user), notice: 'データを削除しました'
   end
 
   private
 
-  def set_vegetable_info
-    @vegetable_type = params[:vegetable_type] || "バジル"
-    
-    case @vegetable_type
-    when "バジル"
-      @default_price_per_kg = 2500 # 100gあたり約250円、1kgあたりに変換
-    when "にんじん"
-      @default_price_per_kg = 434 # 1kgあたり約434円
-    when "トマト"
-      @default_price_per_kg = 791 # 1kgあたり約791円
-    else
-      @default_price_per_kg = 0 # 不明な野菜タイプの場合
+  def harvest_params
+    params.require(:harvest).permit(:amount, :vegetable_type, :price_per_kg)
+  end
+
+  def set_price_per_kg(vegetable_type)
+    case vegetable_type
+    when "バジル" then @price_per_kg = 250
+    when "にんじん" then @price_per_kg = 434
+    when "トマト" then @price_per_kg = 791
+    else @price_per_kg = 0
     end
   end
 
-  def harvest_params
-    params.require(:harvest).permit(:amount, :vegetable_type, :price_per_kg)
+  def render_template(vegetable_type)
+    template = case vegetable_type
+               when "バジル" then "new_basil"
+               when "にんじん" then "new_carrot"
+               when "トマト" then "new_tomato"
+               else "new_default"
+               end
+    render template
   end
 end
