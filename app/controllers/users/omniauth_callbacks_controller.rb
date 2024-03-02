5314e6c4b0116d0f23cdffa3d8e9430e74950d39
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-03-02 15:19:45 +0900

    LINE機能を試すために一回離脱

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,18 +1,17 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def line
-    # LINEからの認証情報をログに出力
-    Rails.logger.info "OmniAuth auth: #{request.env['omniauth.auth'].inspect}"
-
     @user = User.from_omniauth(request.env['omniauth.auth'])
+
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
+      sign_in @user, event: :authentication # ユーザーをサインインさせます
       set_flash_message(:notice, :success, kind: "LINE") if is_navigational_format?
+      redirect_to user_path(@user) # ユーザーのマイページにリダイレクトします
     else
-      session['devise.line_data'] = request.env['omniauth.auth'].except('extra')
+      session['devise.line_data'] = request.env['omniauth.auth'].except('extra') # 必要に応じてセッションを設定
       redirect_to new_user_registration_url, alert: "LINEアカウントから必要な情報を取得できませんでした"
     end
   end
-  
+
   def failure
     redirect_to root_path, alert: "LINEでの認証に失敗しました"
   end
