b5194425917b9b145085dc566ff7fca29aaf8207
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-03-05 10:12:15 +0900

    LINEログイン機能修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :line, ENV['LINE_CHANNEL_ID'], ENV['LINE_CHANNEL_SECRET']
+  provider :line, ENV['LINE_CHANNEL_ID'], ENV['LINE_CHANNEL_SECRET'], scope: 'profile openid email'
 end
