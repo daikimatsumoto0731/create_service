fa7e4ff9c88146c338eb31aab5dfa5d98b1aa2e1
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-03-06 23:35:38 +0900

    LINEログイン機能をできるように実験

--- Gemfile
+++ Gemfile
@@ -13,13 +13,17 @@ gem 'coffee-rails', '~> 4.2'
 gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'devise', '~> 4.8'
+
 gem 'omniauth-line', '<= 2.1.2'
 gem 'omniauth-rails_csrf_protection'
 gem 'devise-i18n'
 gem 'webpacker'
+gem 'dotenv-rails'
+
 gem 'bootstrap', '~> 5.0'
 gem 'jquery-rails'
-gem 'line-bot-api', '~> 1.28'
+gem 'line-bot-api'
+
 gem 'momentjs-rails'
 gem 'httparty'
 gem 'whenever', require: false
@@ -27,7 +31,6 @@ gem 'whenever', require: false
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'dotenv-rails' # dotenv-rails はここでのみ必要と仮定
   gem 'sqlite3', '~> 1.4' # SQLiteをここに移動
 end
 
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,18 +1,31 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def line
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+    basic_action
+  end
 
-    if @user.persisted?
-      sign_in @user, event: :authentication # ユーザーをサインインさせます
-      set_flash_message(:notice, :success, kind: "LINE") if is_navigational_format?
-      redirect_to user_path(@user) # ユーザーのマイページにリダイレクトします
+  private
+
+  def basic_action
+    @omniauth = request.env["omniauth.auth"]
+    if @omniauth.present?
+      @profile = User.find_or_initialize_by(provider: @omniauth["provider"], uid: @omniauth["uid"])
+      # ユーザー情報がデータベースに存在しない場合、新規登録を行う
+      if @profile.new_record?
+        email = @omniauth["info"]["email"].presence || fake_email(@omniauth["uid"], @omniauth["provider"])
+        @profile.assign_attributes(email:, name: @omniauth["info"]["name"],
+                                   password: Devise.friendly_token[0, 20])
+        @profile.save!
+      end
+      # アクセストークンの更新
+      @profile.refresh_access_token(@omniauth)
+      sign_in(:user, @profile)
+      redirect_to user_path(current_user), notice: "ログインしました"
     else
-      session['devise.line_data'] = request.env['omniauth.auth'].except('extra') # 必要に応じてセッションを設定
-      redirect_to new_user_registration_url, alert: "LINEアカウントから必要な情報を取得できませんでした"
+      redirect_to new_user_session_path, alert: "LINE認証に失敗しました"
     end
   end
 
-  def failure
-    redirect_to root_path, alert: "LINEでの認証に失敗しました"
+  def fake_email(uid, provider)
+    "#{uid}-#{provider}-#{SecureRandom.hex(5)}@example.com"
   end
 end
