4ebdd5dff1605899193064e4a8418a47ea681c3d
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-03-06 23:40:28 +0900

    Resolve merge conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,27 +19,4 @@ class User < ApplicationRecord
       user.profile_image = auth.info.image
     end
   end
-<<<<<<< HEAD
-  
-  def social_profile(provider)
-    social_profile.select { |sp| sp.provider == provider.to_s }.first
-  end
-
-  def set_values(omniauth)
-    return if provider.to_s != omniauth["provider"].to_s || uid != omniauth["uid"]
-    credentials = omniauth["credentials"]
-    info = omniauth["info"]
-
-    access_token = credentials["refresh_token"]
-    access_secret = credentials["secret"]
-    credentials = credentials.to_json
-    name = info["name"]
-  end
-
-  def set_value_by_raw_info(raw_info)
-    self.raw_info = raw_info.to_json
-    self.save!
-  end
-=======
->>>>>>> create_19
 end
