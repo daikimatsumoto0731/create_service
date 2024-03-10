ba582843b478ac990a7db6bac54d9c939768d37b
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-03-10 22:42:14 +0900

    もう一度修正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,18 +9,24 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @omniauth = request.env["omniauth.auth"]
     if @omniauth.present?
       @profile = User.find_or_initialize_by(provider: @omniauth["provider"], uid: @omniauth["uid"])
+  
       if @profile.new_record?
         email = @omniauth["info"]["email"].presence || fake_email(@omniauth["uid"], @omniauth["provider"])
-        # デフォルト値を設定
         @profile.assign_attributes(
           email: email,
           name: @omniauth["info"]["name"] || "LINE User",
           password: Devise.friendly_token[0, 20],
-          username: "未設定", # LINEからはusernameを取得できないためデフォルト値を設定
-          prefecture: "未設定" # 県名もデフォルト値を設定
+          # その他必要な属性の設定
         )
+  
+        unless @profile.valid?
+          logger.debug @profile.errors.full_messages
+          # 他のエラー処理（エラーメッセージをユーザーに表示する等）
+        end
+  
         @profile.save!
       end
+  
       # アクセストークンの更新とサインイン
       @profile.refresh_access_token(@omniauth)
       sign_in(:user, @profile)
