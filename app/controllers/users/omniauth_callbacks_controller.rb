632ee772997eb1cfab9c0d704e65a5dbb81b5ee8
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-03-10 22:26:10 +0900

    LINE認証機能の修正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,21 +9,26 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @omniauth = request.env["omniauth.auth"]
     if @omniauth.present?
       @profile = User.find_or_initialize_by(provider: @omniauth["provider"], uid: @omniauth["uid"])
-      # ユーザー情報がデータベースに存在しない場合、新規登録を行う
       if @profile.new_record?
         email = @omniauth["info"]["email"].presence || fake_email(@omniauth["uid"], @omniauth["provider"])
-        @profile.assign_attributes(email:, name: @omniauth["info"]["name"],
-                                   password: Devise.friendly_token[0, 20])
+        # デフォルト値を設定
+        @profile.assign_attributes(
+          email: email,
+          name: @omniauth["info"]["name"] || "LINE User",
+          password: Devise.friendly_token[0, 20],
+          username: "未設定", # LINEからはusernameを取得できないためデフォルト値を設定
+          prefecture: "未設定" # 県名もデフォルト値を設定
+        )
         @profile.save!
       end
-      # アクセストークンの更新
+      # アクセストークンの更新とサインイン
       @profile.refresh_access_token(@omniauth)
       sign_in(:user, @profile)
       redirect_to user_path(current_user), notice: "ログインしました"
     else
       redirect_to new_user_session_path, alert: "LINE認証に失敗しました"
     end
-  end
+  end  
 
   def fake_email(uid, provider)
     "#{uid}-#{provider}-#{SecureRandom.hex(5)}@example.com"
