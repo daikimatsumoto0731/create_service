619d5e952cdfbb890d20a3bf2d8be692eaf08e61
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-03-11 20:47:10 +0900

    マイページ画面での表示を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,18 +13,14 @@ class User < ApplicationRecord
   # OmniAuth認証データからユーザーを検索または作成します
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      # LINEからの情報でユーザー属性を設定
       user.email = auth.info.email.presence || User.generate_email(auth)
       user.password = Devise.friendly_token[0, 20]
       user.username = auth.info.name.presence || "LINE User"
-      user.line_user_id = auth.uid # LINEユーザーIDを保存
-      # デフォルト値を設定
+      user.line_user_id = auth.uid
       user.prefecture = "未設定"
-      # user.profile_image = auth.info.image が必要であれば追加
     end
   end
 
-  # アクセストークンと有効期限を更新
   def refresh_access_token(auth)
     self.access_token = auth.credentials.token
     self.token_expires_at = Time.at(auth.credentials.expires_at) if auth.credentials.expires_at
