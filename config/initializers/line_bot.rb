358a4f347f699bb77ef2b67f4d12fd39b218f869
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-03-12 21:15:22 +0900

    .envファイル修正に伴うコードの修正

--- config/initializers/line_bot.rb
+++ config/initializers/line_bot.rb
@@ -2,6 +2,6 @@ require 'line/bot'
 
 # LINE Botの設定
 Rails.application.config.line_bot_client = Line::Bot::Client.new do |config|
-  config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+  config.channel_secret = ENV['LINE_MESSAGING_API_CHANNEL_SECRET']
   config.channel_token = ENV['LINE_CHANNEL_TOKEN']
 end
