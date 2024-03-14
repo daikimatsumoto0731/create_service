91893777ce132a0e93dc2648cf24e408297453a2
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-03-14 23:00:05 +0900

    Fix Sidekiq Scheduler configuration

--- app/jobs/daily_watering_reminder_job.rb
+++ app/jobs/daily_watering_reminder_job.rb
@@ -1,14 +1,20 @@
-# app/jobs/daily_watering_reminder_job.rb
 class DailyWateringReminderJob
   include Sidekiq::Worker
-    
+      
   def perform
+    Rails.logger.info "DailyWateringReminderJob started"
     User.includes(:user_setting).find_each do |user|
-      # ユーザー設定が存在し、かつ通知を受け取る設定になっている場合のみ通知を送信
       if user.line_user_id.present? && user.user_setting&.should_receive_notifications?
-        LineBotService.send_push_message(user.line_user_id, "水やりの時間です！")
+        Rails.logger.info "Sending push message to user: #{user.id}"
+        begin
+          response = LineBotService.send_push_message(user.line_user_id, "水やりの時間です！")
+          Rails.logger.info "Push message sent successfully: #{response}"
+        rescue => e
+          Rails.logger.error "Error sending push message: #{e.message}"
+        end
       end
     end
+    Rails.logger.info "DailyWateringReminderJob finished"
   end
 end
   
