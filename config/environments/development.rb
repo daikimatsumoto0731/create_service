b74d7c9ddbe86c6507e5f5263752b31d4fe26c19
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-03-15 00:16:33 +0900

    ファイルの修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,15 @@ Rails.application.configure do
   # Mailer settings
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.example.com',
+    port:                 587,
+    user_name:            'your_username',
+    password:             'your_password',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
   config.action_mailer.perform_deliveries = true
   config.hosts.clear
 
