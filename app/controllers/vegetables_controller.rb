dda2d92424271a9af877dc89f34735fcdce38396
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-03-18 00:01:59 +0900

    種まき日の更新日の日付を選択ボタンを押した日付にする

--- app/controllers/vegetables_controller.rb
+++ app/controllers/vegetables_controller.rb
@@ -4,11 +4,14 @@ class VegetablesController < ApplicationController
 
   def schedule
     selected_vegetable = params[:selected_vegetable]
+    selected_date = params[:sowing_date]  # 修正: 選択された日付を取得
 
     if selected_vegetable.blank?
       redirect_to vegetables_path  # 選択されていない場合は野菜選択画面にリダイレクト
     else
       # 選択された野菜をスケジュール画面に渡す
+      # 修正: 選択された日付をセッションに保存
+      session[:selected_date] = selected_date
       redirect_to events_path(selected_vegetable: selected_vegetable)
     end
   end
