664b5db6dce7b32fcf9a9313b5cdcd7cdd7aa217
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-03-27 00:52:01 +0900

    コードの一部修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'line/bot'
 
 class ApplicationController < ActionController::Base
--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class LineBotController < ApplicationController
   protect_from_forgery except: [:callback]
 
