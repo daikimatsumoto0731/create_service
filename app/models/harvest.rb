ad90c8bcf3a6ef1225e2ebd0e0822911c9df01f6
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-04-05 20:06:10 +0900

    モデルコードの修正

--- app/models/harvest.rb
+++ app/models/harvest.rb
@@ -15,21 +15,21 @@ class Harvest < ApplicationRecord
 
   def self.calculate_max_savings_month(user)
     grouped_savings = where(user: user)
-                      .group("strftime('%Y-%m', created_at)")
-                      .select("strftime('%Y-%m', created_at) as month, SUM(amount * price_per_kg) as total_savings")
+                      .group("TO_CHAR(created_at, 'YYYY-MM')")
+                      .select("TO_CHAR(created_at, 'YYYY-MM') as month, SUM(amount * price_per_kg) as total_savings")
                       .order('total_savings DESC')
                       .first
-
+  
     max_savings_info = { month: grouped_savings&.month, total_savings: grouped_savings&.total_savings }
-
+  
     grouped_harvests = where(user: user)
                        .group(:vegetable_type)
                        .select('vegetable_type, SUM(amount) as total_amount')
                        .order('total_amount DESC')
                        .first
-
+  
     max_harvest_info = { vegetable_type: grouped_harvests&.vegetable_type, total_amount: grouped_harvests&.total_amount }
-
+  
     { max_savings_info: max_savings_info, max_harvest_info: max_harvest_info }
-  end
+  end  
 end
