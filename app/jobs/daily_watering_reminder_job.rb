8d8d76a3ac243ec4935e864c2d0068caf3326daf
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-04-22 15:04:11 +0900

    LINE通知と天気情報の連携

--- app/jobs/daily_watering_reminder_job.rb
+++ app/jobs/daily_watering_reminder_job.rb
@@ -1,26 +1,14 @@
 # frozen_string_literal: true
 
+# app/jobs/daily_watering_reminder_job.rb
 class DailyWateringReminderJob
   include Sidekiq::Worker
 
   def perform
-    Rails.logger.info 'DailyWateringReminderJob started'
-    User.joins(:user_setting).find_each do |user|
-      if user.line_user_id.present? && user.user_setting&.should_receive_notifications?
-        # ユーザーの都道府県から天気情報を取得
-        weather_data = WeatherService.fetch_weather(user.prefecture)
-        if weather_data['weather']  # 天気情報が正常に取得できた場合
-          Rails.logger.info "Sending weather-based push message to user: #{user.id}"
-          begin
-            LineBotService.send_weather_message(user.line_user_id, weather_data)
-          rescue StandardError => e
-            Rails.logger.error "Error sending weather-based push message: #{e.message}"
-          end
-        else
-          Rails.logger.error "Failed to fetch weather data for user: #{user.id}"
-        end
+    User.joins(:line_notification_setting).where(line_notification_settings: { receive_notifications: true }).each do |user|
+      if user.line_notification_setting.notification_time.strftime('%H:%M') == Time.current.strftime('%H:%M')
+        LineBotService.send_weather_message(user.line_user_id, WeatherService.fetch_weather(user.prefecture))
       end
     end
-    Rails.logger.info 'DailyWateringReminderJob finished'
   end
 end
