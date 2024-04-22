86f4a947f93835e55625a881c8fd2490d477b5b3
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-04-22 14:49:56 +0900

    マイページ画面に天気を表示

--- app/jobs/daily_watering_reminder_job.rb
+++ app/jobs/daily_watering_reminder_job.rb
@@ -7,12 +7,17 @@ class DailyWateringReminderJob
     Rails.logger.info 'DailyWateringReminderJob started'
     User.joins(:user_setting).find_each do |user|
       if user.line_user_id.present? && user.user_setting&.should_receive_notifications?
-        Rails.logger.info "Sending push message to user: #{user.id}"
-        begin
-          response = LineBotService.send_push_message(user.line_user_id, '水やりの時間です！')
-          Rails.logger.info "Push message sent successfully: #{response}"
-        rescue StandardError => e
-          Rails.logger.error "Error sending push message: #{e.message}"
+        # ユーザーの都道府県から天気情報を取得
+        weather_data = WeatherService.fetch_weather(user.prefecture)
+        if weather_data['weather']  # 天気情報が正常に取得できた場合
+          Rails.logger.info "Sending weather-based push message to user: #{user.id}"
+          begin
+            LineBotService.send_weather_message(user.line_user_id, weather_data)
+          rescue StandardError => e
+            Rails.logger.error "Error sending weather-based push message: #{e.message}"
+          end
+        else
+          Rails.logger.error "Failed to fetch weather data for user: #{user.id}"
         end
       end
     end
