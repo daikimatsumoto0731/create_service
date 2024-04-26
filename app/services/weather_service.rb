268eb2f19d7f9f5c1cb1fc9c5aabccda1088e02c
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-04-26 21:56:22 +0900

    コードの修正

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,19 +1,19 @@
+# WeatherServiceクラス
 require 'net/http'
 require 'uri'
 require 'json'
-require 'cgi' # CGI.escapeを使用するために必要
+require 'cgi'
 
 class WeatherService
   BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
   @@client = Net::HTTP.new(URI(BASE_URL).host, URI(BASE_URL).port)
   @@client.use_ssl = true
+  API_KEY = ENV['OPENWEATHER_API_KEY'] # .envファイルからAPIキーを取得
   
-  # 特定の都市の天気情報を取得するクラスメソッド
   def self.fetch_weather(city)
     escaped_city = CGI.escape(city)
-    uri = URI("#{BASE_URL}?q=#{escaped_city}&appid=#{ENV['OPENWEATHER_API_KEY']}&units=metric&lang=ja")  # 日本語のレスポンスを指定
+    uri = URI("#{BASE_URL}?q=#{escaped_city}&appid=#{API_KEY}&units=metric&lang=ja")
   
-    # APIリクエストのエラーハンドリング
     begin
       request = Net::HTTP::Get.new(uri)
       request["Accept"] = "application/json"
@@ -23,13 +23,10 @@ class WeatherService
       when Net::HTTPSuccess then
         JSON.parse(response.body)
       else
-        # 他のHTTPエラーを適切にハンドル
-        { error: "HTTP Error: #{response.code} #{response.message}" }
+        { error: "天気情報の取得に失敗しました: #{response.code} #{response.message}" }
       end
     rescue StandardError => e
-      # エラーロギングや別のエラー対応をここに記述
-      { error: "Failed to fetch weather data: #{e.message}" }
+      { error: "天気情報の取得に失敗しました: #{e.message}" }
     end
   end
 end
-  
