a39defbf2f168c7bfae52f658de611a352141d2d
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-04-26 23:23:01 +0900

    コードの修正

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,20 +1,27 @@
 # frozen_string_literal: true
 
 class LineBotController < ApplicationController
-  protect_from_forgery with: :null_session
+  protect_from_forgery except: [:callback]
 
+  # LINE Botがメッセージを受信したときの処理
   def callback
     body = request.body.read
+
     signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      return head :bad_request
+    unless line_bot_client.validate_signature(body, signature)
+      head :bad_request
+      return
     end
 
-    events = client.parse_events_from(body)
+    events = line_bot_client.parse_events_from(body)
+
     events.each do |event|
       case event
-      when Line::Bot::Event::Follow
-        handle_follow_event(event)
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          reply_message(event['replyToken'], '水やりの時間です！')
+        end
       end
     end
 
@@ -23,18 +30,16 @@ class LineBotController < ApplicationController
 
   private
 
-  def handle_follow_event(event)
-    user_id = event['source']['userId']
-    User.find_or_create_by(line_user_id: user_id) do |user|
-      user.line_notification_setting ||= user.build_line_notification_setting(receive_notifications: true)
-      user.save!
-    end
+  def line_bot_client
+    @line_bot_client ||= Rails.application.config.line_bot_client
   end
 
-  def client
-    @client ||= Line::Bot::Client.new do |config|
-      config.channel_secret = ENV['LINE_MESSAGING_API_CHANNEL_SECRET']
-      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-    end
+  # LINEメッセージを返信する処理
+  def reply_message(reply_token, text)
+    message = {
+      type: 'text',
+      text: text
+    }
+    line_bot_client.reply_message(reply_token, message)
   end
 end
