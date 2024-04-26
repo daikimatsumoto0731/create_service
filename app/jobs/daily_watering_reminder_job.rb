22c6dff4f93fd26922ca380c8ffbdcd96b1093b2
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-04-26 21:56:22 +0900

    コードの修正

--- app/jobs/daily_watering_reminder_job.rb
+++ app/jobs/daily_watering_reminder_job.rb
@@ -6,9 +6,7 @@ class DailyWateringReminderJob
   include Sidekiq::Worker
 
   def perform
-    # 通知を受け取るユーザーをすべて取得
     User.joins(:line_notification_setting).where(line_notification_settings: { receive_notifications: true }).each do |user|
-      # 天気データを取得し、メッセージを送信
       weather_data = WeatherService.fetch_weather(user.prefecture)
       LineBotService.send_weather_message(user.line_user_id, weather_data)
     end
