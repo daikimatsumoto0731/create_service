3c830f66a74ed7b83f50c7d6c0e35b617bc1ec02
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-04-30 00:44:53 +0900

    コードの修正

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -6,8 +6,8 @@ class LineBotController < ApplicationController
   # LINE Botがメッセージを受信したときの処理
   def callback
     body = request.body.read
-
     signature = request.env['HTTP_X_LINE_SIGNATURE']
+
     unless line_bot_client.validate_signature(body, signature)
       head :bad_request
       return
@@ -18,10 +18,9 @@ class LineBotController < ApplicationController
     events.each do |event|
       case event
       when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          reply_message(event['replyToken'], '水やりの時間です！')
-        end
+        handle_message_event(event)
+      when Line::Bot::Event::Follow
+        handle_follow_event(event)
       end
     end
 
@@ -31,7 +30,25 @@ class LineBotController < ApplicationController
   private
 
   def line_bot_client
-    @line_bot_client ||= Rails.application.config.line_bot_client
+    @line_bot_client ||= Line::Bot::Client.new do |config|
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+    end
+  end
+
+  # LINEメッセージを受信したときの処理
+  def handle_message_event(event)
+    case event.type
+    when Line::Bot::Event::MessageType::Text
+      reply_message(event['replyToken'], '水やりの時間です！')
+    end
+  end
+
+  # ユーザーが友達追加したときのイベントを処理する
+  def handle_follow_event(event)
+    user_id = event['source']['userId']
+    User.find_or_create_by(line_user_id: user_id)  # ユーザーIDをデータベースに保存する処理
+    reply_message(event['replyToken'], "友達登録ありがとうございます！")
   end
 
   # LINEメッセージを返信する処理
