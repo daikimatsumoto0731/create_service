efc58bc73d26260501b0ffc13ae00cc99785acb2
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-05-21 22:21:57 +0900

    画像解析とアドバイス機能の設置

---
+++ app/services/libre_translate.rb
@@ -0,0 +1,26 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+class LibreTranslate
+  BASE_URL = 'http://localhost:5001' # Dockerコンテナのポート
+
+  def self.translate(text, source_lang = 'en', target_lang = 'ja')
+    uri = URI("#{BASE_URL}/translate")
+    response = Net::HTTP.post_form(uri, {
+      'q' => text,
+      'source' => source_lang,
+      'target' => target_lang,
+      'format' => 'text'
+    })
+
+    response.body.force_encoding('UTF-8') # レスポンスのエンコーディングをUTF-8に設定
+    result = JSON.parse(response.body)
+    translated_text = result['translatedText']
+    translated_text.force_encoding('UTF-8') # エンコーディングを明示的に設定
+    translated_text
+  rescue StandardError => e
+    puts "Translation error: #{e.message}"
+    nil
+  end
+end
