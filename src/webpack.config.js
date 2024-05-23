8ced24bdabb03c3a85907013371e3eea5345c5ed
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-05-24 02:07:19 +0900

    Install webpack globally

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -2,20 +2,20 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js', // エントリーポイントのパスを設定
+  entry: './src/index.js',
   output: {
-    filename: 'bundle.js', // 出力ファイルの名前を設定
-    path: path.resolve(__dirname, 'dist'), // 出力先ディレクトリを設定
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
-        test: /\.js$/, // JavaScript ファイルに対する処理を指定
-        exclude: /node_modules/, // node_modules ディレクトリ内のファイルは処理対象外
+        test: /\.js$/,
+        exclude: /node_modules/,
         use: {
-          loader: 'babel-loader', // Babel を使用して ECMAScript 新機能をトランスパイル
+          loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'], // Babel のプリセットを指定
+            presets: ['@babel/preset-env'],
           },
         },
       },
