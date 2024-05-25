66b04ca8efe9d4861c71791c5a5d10ffb099d57f
Matsumoto Daiki <dai.dai.2002@icloud.com>
2024-05-25 17:04:47 +0900

    Fix temporary file creation for Google Cloud credentials

--- config/initializers/google_cloud.rb
+++ config/initializers/google_cloud.rb
@@ -2,12 +2,16 @@
 
 if ENV['GOOGLE_APPLICATION_CREDENTIALS_JSON']
   require 'tempfile'
-  temp_file = Tempfile.new('google_application_credentials')
-  temp_file.write(ENV['GOOGLE_APPLICATION_CREDENTIALS_JSON'])
-  temp_file.rewind
-  ENV['GOOGLE_APPLICATION_CREDENTIALS'] = temp_file.path
-  Rails.logger.info "Temporary credentials file created at: #{temp_file.path}"
+  begin
+    temp_file = Tempfile.new('google_application_credentials')
+    temp_file.write(ENV['GOOGLE_APPLICATION_CREDENTIALS_JSON'])
+    temp_file.rewind
+    ENV['GOOGLE_APPLICATION_CREDENTIALS'] = temp_file.path
+    Rails.logger.info "Temporary credentials file created at: #{temp_file.path}"
+  rescue StandardError => e
+    Rails.logger.error "Failed to create temporary credentials file: #{e.message}"
+  end
 else
-  Rails.logger.error "GOOGLE_APPLICATION_CREDENTIALS_JSON is not set."
+  Rails.logger.error "GOOGLE_APPLICATIONS_CREDENTIALS_JSON is not set."
 end
   
